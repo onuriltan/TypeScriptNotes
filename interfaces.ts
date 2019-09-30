cd240359964990344fc7f439ce7c62018d4c627f
onuriltan <onur.iltan@gmail.com>
2019-09-30 17:43:57 +0300

    add interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -2,18 +2,29 @@ interface Vehicle {
   name: string;
   year: Date;
   broken: boolean;
+  summary(): string;
+}
+interface Reportable {
+  summary(): string;
 }
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.name);
-  console.log(vehicle.year);
-  console.log(vehicle.broken);
+  console.log(vehicle.summary);
+};
+
+// Code Reuse
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary);
 };
 
 const oldCivic = {
   name: 'civic',
   year: new Date(),
-  broken: true
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
 printVehicle(oldCivic);
+printSummary(oldCivic);
